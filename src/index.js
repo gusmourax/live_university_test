2ab3edbc9c967f592cb9c0c6c7bb8a8bfaf31485
WinnerJava <gustavomoura5555@outlook.com>
2020-07-22 01:47:39 -0300

    First Commit

--- src/index.js
+++ src/index.js
@@ -15,4 +15,4 @@ app.get('/', (req, res) => res.render('index', {
     pages: Math.ceil(req.query.quantity / 3)
 }));
 
-app.listen(3333, () => console.log('App running on port 3333'));
+app.listen(process.env.PORT || 3333);
