76d20f0cfc25d56f192f9076e59ed978fbf77668
WinnerJava <gustavomoura5555@outlook.com>
2020-07-22 02:32:35 -0300

    First Commit

--- src/index.js
+++ src/index.js
@@ -3,16 +3,23 @@ const path = require('path');
 
 const app = express();
 
-
+//Set static directory public
 app.use(express.static(__dirname + '/public'));
 
+//Set views directory
 app.set('views', path.join(__dirname, '/views/'));
+
+//Set view engine to ejs
 app.set('view engine', 'ejs');
 
+//Get / to render the index ejs file
 app.get('/', (req, res) => res.render('index', {
+    //Number of quantity
     quantity: req.query.quantity || 0,
+    //Number of page if exists or 1
     page: req.query.page || 1,
-    pages: Math.ceil(req.query.quantity / 3)
+    //Number of total pages
+    pages: Math.ceil(req.query.quantity / 3) //Quantity divided by number of elements per page
 }));
 
 app.listen(process.env.PORT || 3333);
--- src/public/js/index.js
+++ src/public/js/index.js
@@ -1,40 +1,59 @@
 $(document).ready(function() {
-    var open = false;
+    //hidden-dropdown-div isOpen variable
+    var isOpen = false;
     $('.option').on('click', function() {
-        if (!open) {
-            open = !open;
+        if (!isOpen) {
+            //Equivalent to isOpen = true
+            isOpen = !isOpen;
+            //Open the hidden-dropdown-container
             $('.hidden-dropdown-container').css({ left: 280 });
         } else {
-            open = !open;
+            //Equivalent to isOpen = true
+            isOpen = !isOpen;
+            //Close the hidden-dropdown-container
             $('.hidden-dropdown-container').css({ left: 0 });
         }
     })
     $(window).on('click', function(e) {
+        //Check if user click outside of hidden-dropdown-container and option button
         if (!document.getElementById('hidden-dropdown').contains(e.target) &&
             !document.getElementById('option-btn').contains(e.target)) {
-            if (open) {
+            //Check if the hidden-dropdown-container is open
+            if (isOpen) {
+                //Close if user click outside of hidden-dropdown-container
                 $('.hidden-dropdown-container').css({ left: 0 });
-                open = !open;
+                //Equivalent to isOpen = false
+                isOpen = !isOpen;
             }
         }
     })
 
     function updateQueryContent() {
+        //Get value of dropdown menu from sessionStorage or A    
         var value = sessionStorage.getItem('item') || 'A';
+        //Select option with sessionStorage value
         $('#item-select').children(`option[value=${value}]`).attr('selected', true);
+        //Clear the span to append dropdown option selected value
         $('.content-option').empty();
+        //Append dropdown option selected value to span
         $('.content-option').append(`${value}`);
     }
 
+    //Call function on document load
     updateQueryContent();
 
     $('#item-select').on('change', function() {
+        //Set sessionStorage item with dropdown option selected value
         sessionStorage.setItem('item', $('#item-select').children('option:selected').val());
         var urlParams = new URLSearchParams(window.location.search);
+        //Check if exists parameter quantity and if parameter quantity is > 0
         if (urlParams.get('quantity') && urlParams.get('quantity') > 0) {
+            //Clear span with the content option
             $('.content-option').empty();
+            //Call function to update the query content
             updateQueryContent();
         } else {
+            //If quantity is not set, make background of input red
             $('#quantity-input').css({ background: '#ff6b6b' })
         }
     });
